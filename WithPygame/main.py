e688eaececeafd3c3ad30e7703c49bcf722f9dd2
[Iñaki Rosello] <[irosello@outlook.com.ar]>
2024-02-29 23:36:27 -0300

    Agregue el funcionamiento del automata celular: Brian `s Brain

--- WithPygame/main.py
+++ WithPygame/main.py
@@ -8,6 +8,9 @@ DYING_CELLS = (169,169,169)
 COLOR_BG = (0,0,0)
 COLOR_GRID = (50,50,50)
 
+REFFRACTORY_CELL = (255,0,0)
+FIRING_CELL = CELLS_ALIVE
+
 def ConwayGoL(screen, cell, size, progress = False):
     updated_cells = np.zeros((cell.shape[0],cell.shape[1]))
 
@@ -58,16 +61,44 @@ def DayAndNight(screen, cell, size, progress = False):
     
     return  updated_cells
 
+def Brian_Brain(screen, cell, size, progress = False):
+    updated_cells = np.zeros((cell.shape[0],cell.shape[1]))
+    
+    for row,col in np.ndindex(cell.shape):
+        alive = np.nansum(cell[row-1:row+2,col-1:col+2]) - cell[row,col]
+        color = COLOR_BG if cell[row,col] == 0 else (CELLS_ALIVE if cell[row,col] == 1 else REFFRACTORY_CELL)
+
+        if cell[row,col] == 1:
+            updated_cells[row,col] = None
+            if progress:
+                color = REFFRACTORY_CELL
+
+        elif cell[row, col] == None:
+            updated_cells[row,col] = 0
+            if progress:
+                color = DYING_CELLS
+
+        else:
+            if alive == 2:
+                updated_cells[row,col] = 1
+                if progress:
+                    color = CELLS_ALIVE
+                    
+        pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
+    return  updated_cells
+
 def Choice(Decision, screen, cell, size, progress = False):
-    if Decision == "DayAndNight":
+    if Decision.upper() == "DAN":
         return DayAndNight(screen, cell, size, progress)
-    elif Decision == "ConwayGoL":
+    elif Decision.upper() == "CGL":
         return ConwayGoL(screen, cell, size, progress)
+    elif Decision.upper() == "BB":
+        return Brian_Brain(screen, cell, size, progress)
 
 def main():
     print("Welcome, pick a one Cellular Automaton between these four to simulate. Write your desicion as indicates the parentheses of each.")
-    Desicion = input("Conway`s Game Of Life (\"ConwayGoL\"), Day And Night (\"DayAndNight\")")
-
+    Desicion = input("Conway`s Game Of Life (\"CGL\"), Day And Night (\"DAN\"), Brian`s Brain (\"BB\") o Langton`s Ant(\"LA\"): ") 
+    RoundBB = 0
     pygame.init()
     screen = pygame.display.set_mode((800,600))
     
@@ -87,7 +118,7 @@ def main():
                 return
             
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
+                if event.key == pygame.K_SPACE: 
                     running = not running
                     Choice(Desicion, screen, cell, 10)
                     pygame.display.update()
@@ -108,6 +139,12 @@ def main():
             if pygame.mouse.get_pressed()[0]:
                 pos = pygame.mouse.get_pos()
                 cell[pos[1]//10,pos[0]//10] = 1
+                Choice(Desicion, screen, cell, 10)
+                pygame.display.update()
+
+            if pygame.mouse.get_pressed()[2] and Desicion.upper() == "BB":
+                pos = pygame.mouse.get_pos()
+                cell[pos[1]//10,pos[0]//10] = None
                 Choice(Desicion, screen, cell, 10)
                 pygame.display.update()
 

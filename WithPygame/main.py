8f696fc1dcc3ebefaee469de74b6cbbed6af325f
[Iñaki Rosello] <[irosello@outlook.com.ar]>
2024-03-01 19:44:06 -0300

    Modificacion del codigo para poner las funciones de cada automata celular en un archivo aparte

--- WithPygame/main.py
+++ WithPygame/main.py
@@ -1,158 +1,78 @@
-import random
-import numpy as np
+from FunctionPackage import Functions
 import time
-import pygame
+import random
 
-CELLS_ALIVE = (255,255,255)
-DYING_CELLS = (169,169,169)
-COLOR_BG = (0,0,0)
-COLOR_GRID = (50,50,50)
-
-REFFRACTORY_CELL = (255,0,0)
-FIRING_CELL = CELLS_ALIVE
-
-def ConwayGoL(screen, cell, size, progress = False):
-    updated_cells = np.zeros((cell.shape[0],cell.shape[1]))
-
-    for row,col in np.ndindex(cell.shape):
-        alive = np.sum(cell[row-1:row+2,col-1:col+2]) - cell[row,col]
-        color = COLOR_BG if cell[row,col] == 0 else CELLS_ALIVE
-
-        if cell[row,col] == 1:
-            if alive > 3 or alive < 2:
-                if progress:
-                    color = DYING_CELLS
-            elif 2 <= alive <= 3:
-                updated_cells[row,col] = 1
-                if progress:
-                    color = CELLS_ALIVE
-        else:
-            if alive == 3:
-                updated_cells[row,col] = 1
-                if progress:
-                    color = CELLS_ALIVE
-
-        pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
-    
-    return  updated_cells
-
-def DayAndNight(screen, cell, size, progress = False):
-    updated_cells = np.zeros((cell.shape[0],cell.shape[1]))
-
-    for row,col in np.ndindex(cell.shape):
-        alive = np.sum(cell[row-1:row+2,col-1:col+2]) - cell[row,col]
-        color = COLOR_BG if cell[row,col] == 0 else CELLS_ALIVE
-
-        if cell[row,col] == 1:
-            if alive < 3 or (alive == 4 or alive == 5):
-                if progress:
-                    color = DYING_CELLS
-            elif alive >= 6 or alive == 3:
-                updated_cells[row,col] = 1
-                if progress:
-                    color = CELLS_ALIVE
-        else:
-            if alive >= 3 and alive != 5:
-                updated_cells[row,col] = 1
-                if progress:
-                    color = CELLS_ALIVE
-
-        pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
-    
-    return  updated_cells
-
-def Brian_Brain(screen, cell, size, progress = False):
-    updated_cells = np.zeros((cell.shape[0],cell.shape[1]))
-    
-    for row,col in np.ndindex(cell.shape):
-        alive = np.nansum(cell[row-1:row+2,col-1:col+2]) - cell[row,col]
-        color = COLOR_BG if cell[row,col] == 0 else (CELLS_ALIVE if cell[row,col] == 1 else REFFRACTORY_CELL)
-
-        if cell[row,col] == 1:
-            updated_cells[row,col] = None
-            if progress:
-                color = REFFRACTORY_CELL
-
-        elif cell[row, col] == None:
-            updated_cells[row,col] = 0
-            if progress:
-                color = DYING_CELLS
-
-        else:
-            if alive == 2:
-                updated_cells[row,col] = 1
-                if progress:
-                    color = CELLS_ALIVE
-                    
-        pygame.draw.rect(screen, color, (col * size, row * size, size - 1, size - 1))
-    return  updated_cells
-
-def Choice(Decision, screen, cell, size, progress = False):
-    if Decision.upper() == "DAN":
-        return DayAndNight(screen, cell, size, progress)
-    elif Decision.upper() == "CGL":
-        return ConwayGoL(screen, cell, size, progress)
-    elif Decision.upper() == "BB":
-        return Brian_Brain(screen, cell, size, progress)
+def Choice(Decision, screen, cell, size,  progress = False, Posx = 1, Posy = 1, Direction = 0):
+    while True:
+        if Decision.upper() == "DAN":
+            return Functions.DayAndNight(screen, cell, size, progress)
+        elif Decision.upper() == "CGL":
+            return Functions.ConwayGoL(screen, cell, size, progress)
+        elif Decision.upper() == "BB":
+            return Functions.Brian_Brain(screen, cell, size, progress)
+        elif Decision.upper() == "LA":
+            return Functions.Langton_Ant(screen, cell, size, Direction, Posx, Posy, progress)
 
 def main():
     print("Welcome, pick a one Cellular Automaton between these four to simulate. Write your desicion as indicates the parentheses of each.")
-    Desicion = input("Conway`s Game Of Life (\"CGL\"), Day And Night (\"DAN\"), Brian`s Brain (\"BB\") o Langton`s Ant(\"LA\"): ") 
-    RoundBB = 0
-    pygame.init()
-    screen = pygame.display.set_mode((800,600))
+    while True:
+        Desicion = input("Conway`s Game Of Life (\"CGL\"), Day And Night (\"DAN\"), Brian`s Brain (\"BB\") o Langton`s Ant(\"LA\"): ")
+        if Desicion.upper() != "DAN" and Desicion.upper() != "CGL" and Desicion.upper() != "LA" and Desicion.upper() != "BB":
+            print("La opcion que acabas de introducir no corresponde a ninguna de las opciones disponibles.\nVuelve a intentarlo")
+        else:
+            break
+    Functions.pygame.init()
+    screen = Functions.pygame.display.set_mode((800,600))
     
-    cell = np.zeros((60,80))
-    screen.fill(COLOR_GRID)
+    cell = Functions.np.zeros((60,80))
+    screen.fill(Functions.COLOR_GRID)
     Choice(Desicion, screen, cell, 10)
 
-    pygame.display.flip()
-    pygame.display.update()
+    Functions.pygame.display.flip()
+    Functions.pygame.display.update()
 
     running = False
 
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
+        for event in Functions.pygame.event.get():
+            if event.type == Functions.pygame.QUIT:
+                Functions.pygame.quit()
                 return
             
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE: 
+            elif event.type == Functions.pygame.KEYDOWN:
+                if event.key == Functions.pygame.K_SPACE: 
                     running = not running
                     Choice(Desicion, screen, cell, 10)
-                    pygame.display.update()
+                    Functions.pygame.display.update()
 
-                if event.key == pygame.K_RETURN:
+                if event.key == Functions.pygame.K_RETURN:
                     n = 0
                     while True:
                         pos = ((random.randint(0,799)),(random.randint(0,599)))
                         cell[pos[1]//10,pos[0]//10] = 1
                         Choice(Desicion, screen, cell, 10)
-                        pygame.display.update()
+                        Functions.pygame.display.update()
                         n += 1
                         if n == 1200:
                             break
 
-                    
-
-            if pygame.mouse.get_pressed()[0]:
-                pos = pygame.mouse.get_pos()
+            if Functions.pygame.mouse.get_pressed()[0]:
+                pos = Functions.pygame.mouse.get_pos()
                 cell[pos[1]//10,pos[0]//10] = 1
                 Choice(Desicion, screen, cell, 10)
-                pygame.display.update()
+                Functions.pygame.display.update()
 
-            if pygame.mouse.get_pressed()[2] and Desicion.upper() == "BB":
-                pos = pygame.mouse.get_pos()
+            if Functions.pygame.mouse.get_pressed()[2] and Desicion.upper() == "BB":
+                pos = Functions.pygame.mouse.get_pos()
                 cell[pos[1]//10,pos[0]//10] = None
                 Choice(Desicion, screen, cell, 10)
-                pygame.display.update()
+                Functions.pygame.display.update()
 
-        screen.fill(COLOR_GRID)
+        screen.fill(Functions.COLOR_GRID)
 
         if running:
             cell = Choice(Desicion, screen, cell, 10, progress = True)
-            pygame.display.update()
+            Functions.pygame.display.update()
         time.sleep(0.001)
 
 if __name__ == "__main__":
